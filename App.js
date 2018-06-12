0a57dfa73c48d3b2b762aba7835e030ebc0509b7
adrian-casanova <adrianfran2515@gmail.com>
2018-06-12 14:42:54 -0400

    Build Completed

--- App.js
+++ App.js
@@ -1,174 +1,179 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  PixelRatio,
-  Button,
-  CameraRoll
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    PixelRatio,
+    Button,
+    CameraRoll,
+    Dimensions
 } from 'react-native';
 import Card from './src/components/Card'
 import Header from './src/components/Header';
 import ImagePicker from 'react-native-image-picker'
 // import {Constants, takeSnapshotAsync} from 'expo'
-import {captureScreen} from 'react-native-view-shot'
+import { captureScreen } from 'react-native-view-shot'
 
 
+var { height, width } = Dimensions.get('window')
 
+var capitureScreen = () => {
+    alert('Screenshot Successful!')
+    captureScreen({
+        format: "png",
+        quality: 1,
+    }).then(
+        uri => CameraRoll.saveToCameraRoll(uri, 'photo'),
+        error => console.error("Oops, snapshot failed", error)
 
-var capitureScreen = ()=>{
-    
-  captureScreen({
-      format: "png",
-      quality: 1,
-      }).then(
-      uri => CameraRoll.saveToCameraRoll(uri, 'photo'),
-      error => console.error("Oops, snapshot failed", error)
-       
-      );
-     
+    );
+
 }
-   
+
 
 
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      ImageSource : 'null',
-      ImageSourceTwo : 'null',
-      pikachu : './assets/pikachu.png',
-      cameraRollUri : 'null'
+        this.state = {
+            ImageSource: null,
+            ImageSourceTwo: null,
+            pikachu: './assets/pikachu.png',
+            cameraRollUri: 'null'
+        }
     }
-  }
 
 
-//Function for opening up Camera Roll and Selecting Image
-  selectPhotoTapped() {
-    const options = {
-      quality: 1.0,
-      maxWidth: 500,
-      maxHeight: 500,
-      storageOptions: {
-        skipBackup: true
-      }
-    };
-    ImagePicker.showImagePicker(options, (response) => {
-      console.log('Response = ', response);
+    //Function for opening up Camera Roll and Selecting Image
+    selectPhotoTapped() {
+        const options = {
+            quality: 1.0,
+            maxWidth: 500,
+            maxHeight: 500,
+            storageOptions: {
+                skipBackup: true
+            }
+        };
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
 
-      if (response.didCancel) {
-        console.log('User cancelled photo picker');
-      }
-      else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      }
-      else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      }
-      else {
-        let source = { uri: response.uri };
+            if (response.didCancel) {
+                console.log('User cancelled photo picker');
+            }
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            }
+            else {
+                let source = { uri: response.uri };
 
-      
 
-        this.setState({
 
-          ImageSource: source
+                this.setState({
 
+                    ImageSource: source
+
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  selectPhotoTappedTwo() {
-    const options = {
-      quality: 1.0,
-      maxWidth: 500,
-      maxHeight: 500,
-      storageOptions: {
-        skipBackup: true
-      }
-    };
-    ImagePicker.showImagePicker(options, (response) => {
-      console.log('Response = ', response);
+    selectPhotoTappedTwo() {
+        const options = {
+            quality: 1.0,
+            maxWidth: 500,
+            maxHeight: 500,
+            storageOptions: {
+                skipBackup: true
+            }
+        };
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
 
-      if (response.didCancel) {
-        console.log('User cancelled photo picker');
-      }
-      else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      }
-      else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      }
-      else {
-        let source = { uri: response.uri };
+            if (response.didCancel) {
+                console.log('User cancelled photo picker');
+            }
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            }
+            else {
+                let source = { uri: response.uri };
 
-        
 
-        this.setState({
 
-          ImageSourceTwo: source
+                this.setState({
 
+                    ImageSourceTwo: source
+
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
 
-  render() {
-    return (
-      <View style = {styles.ViewStyle} > 
-        <Header placeholderText = {'Before'}/>
-        <TouchableOpacity  onPress={this.selectPhotoTapped.bind(this)}>
-        <View style={styles.ImageContainer}>
- 
-         { this.state.ImageSource === null ? <Text>Select a Photo</Text> :
-        <Image style={styles.ImageContainer} source={this.state.ImageSource} />
-          }
+    render() {
+        return (
+            <View style={styles.ViewStyle} >
+                <Header placeholderText={'Before'} deviceHeight={height} />
+                <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+                    <View style={styles.ImageContainer}>
 
-        </View>
-        </TouchableOpacity>
-        <Header placeholderText = {'After'}/>
-      <TouchableOpacity onPress={this.selectPhotoTappedTwo.bind(this)}>
-      <View style={styles.ImageContainer}>
- 
- { this.state.ImageSourceTwo === null ? <Text>Select a Photo</Text> :
-<Image style={styles.ImageContainer} source={this.state.ImageSourceTwo} />
-  }
+                        {this.state.ImageSource === null ? <Text>Select a Photo</Text> :
+                            <Image style={styles.ImageContainer} source={this.state.ImageSource} />
+                        }
 
-</View>
-      </TouchableOpacity>
-      <Button title = 'Save' onPress = {capitureScreen}/>
-      </View>
-    );
-  }
+                    </View>
+                </TouchableOpacity>
+                <Header placeholderText={'After'} />
+                <TouchableOpacity onPress={this.selectPhotoTappedTwo.bind(this)}>
+                    <View style={styles.ImageContainer}>
+
+                        {this.state.ImageSourceTwo === null ? <Text>Select a Photo</Text> :
+                            <Image style={styles.ImageContainer} source={this.state.ImageSourceTwo} />
+                        }
+
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={capitureScreen} >
+                    <Image source={require('./assets/share.png')} style={{ height: 50, width: 50 }} />
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 
 const styles = {
-  ViewStyle : {
-    justifyContent : 'center',
-    alignItems : 'center',
-    marginBottom : 60
-  },
+    ViewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: height,
+        marginBottom: 60,
+        backgroundColor: '#EAEDED'
+    },
 
-  ImageContainer: {
-    borderRadius: 10,
-    width: 250,
-    height: 250,
-    borderColor: '#9B9B9B',
-    borderWidth: 1 / PixelRatio.get(),
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#B3B3B3',
-    
-  },
+    ImageContainer: {
+        borderRadius: 10,
+        width: 250,
+        height: 250,
+        borderColor: '#9B9B9B',
+        borderWidth: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#E8F6F3',
+
+    },
 
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
 
 
 const Header = (props) => {
-    const {ViewStyle, TextStyle} = styles;
-    return(
+    const { ViewStyle, TextStyle } = styles;
+    const height = props.deviceHeight
+    return (
 
-        <View style = {ViewStyle}> 
-            <TextInput style = {TextStyle} placeholder = {props.placeholderText}/>
+        <View style={ViewStyle}>
+            <TextInput underlineColorAndroid='rgba(0,0,0,0)' style={TextStyle} placeholder={props.placeholderText} />
         </View>
 
 
@@ -16,18 +17,22 @@ const Header = (props) => {
 }
 
 const styles = {
-    ViewStyle : {
-        justifyContent : 'center',
-        alignItems : 'center',
-        marginTop : 20,
+    ViewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: this.height,
         borderWidth: 2,
-        
+
+
     },
 
-    TextStyle : {
-        fontSize : 24,
-       width : 300,
-       textAlign : 'center'
+    TextStyle: {
+        fontSize: 24,
+        height: 45,
+        width: 300,
+
+        textAlign: 'center',
+        backgroundColor: '#D6EAF8'
     }
 
 }
